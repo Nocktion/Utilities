72a1935bccf98531d379604ce67b8a6912f5998d
Nocktion <hdomi@nockdev.net>
2021-11-17 13:36:03 +0100

    Adde MSSQL.cs

---
+++ MSSQL.cs
@@ -0,0 +1,49 @@
+﻿using Nockdev.Database.Data;
+using System.Data.SqlClient;
+
+namespace Nockdev.Database
+{
+    public class MSSQL
+    {
+        public SqlConnection conn;
+
+        public SqlConnection CreateConnection(string server, string username, string password)
+        {
+            conn = new SqlConnection(string.Concat("Data Source=", server, ";User ID=", username, ";Password=", password, ";ApplicationIntent=ReadWrite;"));
+            conn.Open();
+            return conn;
+        }
+
+        public SqlConnection CreateConnection(string server, string database, string username, string password)
+        {
+            conn = new SqlConnection(string.Concat("Data Source=", server, ";Database=", database, ";User ID=", username, ";Password=", password, ";ApplicationIntent=ReadWrite;"));
+            conn.Open();
+            return conn;
+        }
+
+        ~MSSQL()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+        public SqlDataReader QueryRead(string query)
+        {
+            SqlCommand cmd = new SqlCommand(query);
+            return cmd.ExecuteReader();
+        }
+
+        public void Query(string query)
+        {
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Close()
+        {
+            conn.Close();
+        }
+    }
+}
