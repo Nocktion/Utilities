5ed4065b180f562a5e1ca155a28683f1873eaa47
Nocktion <dominik.hagyba@nockdev.net>
2023-01-18 21:43:28 +0100

    Update MSSQL.cs

--- MSSQL.cs
+++ MSSQL.cs
@@ -1,46 +1,45 @@
-using Nockdev.Database.Data;
 using System.Data.SqlClient;
 
-    public class MSSQL
+public class MSSQL
+{
+    public SqlConnection conn;
+
+    public SqlConnection CreateConnection(string server, string username, string password)
     {
-        public SqlConnection conn;
+        conn = new SqlConnection(string.Concat("Data Source=", server, ";User ID=", username, ";Password=", password, ";ApplicationIntent=ReadWrite;"));
+        conn.Open();
+        return conn;
+    }
 
-        public SqlConnection CreateConnection(string server, string username, string password)
-        {
-            conn = new SqlConnection(string.Concat("Data Source=", server, ";User ID=", username, ";Password=", password, ";ApplicationIntent=ReadWrite;"));
-            conn.Open();
-            return conn;
-        }
+    public SqlConnection CreateConnection(string server, string database, string username, string password)
+    {
+        conn = new SqlConnection(string.Concat("Data Source=", server, ";Database=", database, ";User ID=", username, ";Password=", password, ";ApplicationIntent=ReadWrite;"));
+        conn.Open();
+        return conn;
+    }
 
-        public SqlConnection CreateConnection(string server, string database, string username, string password)
-        {
-            conn = new SqlConnection(string.Concat("Data Source=", server, ";Database=", database, ";User ID=", username, ";Password=", password, ";ApplicationIntent=ReadWrite;"));
-            conn.Open();
-            return conn;
-        }
-
-        ~MSSQL()
-        {
-            if (conn.State == System.Data.ConnectionState.Open)
-            {
-                conn.Close();
-            }
-        }
-
-        public SqlDataReader QueryRead(string query)
-        {
-            SqlCommand cmd = new SqlCommand(query);
-            return cmd.ExecuteReader();
-        }
-
-        public int Query(string query)
-        {
-            SqlCommand cmd = new SqlCommand(query);
-            return cmd.ExecuteNonQuery();
-        }
-
-        public void Close()
+    ~MSSQL()
+    {
+        if (conn.State == System.Data.ConnectionState.Open)
         {
             conn.Close();
         }
     }
+
+    public SqlDataReader QueryRead(string query)
+    {
+        SqlCommand cmd = new SqlCommand(query);
+        return cmd.ExecuteReader();
+    }
+
+    public int Query(string query)
+    {
+        SqlCommand cmd = new SqlCommand(query);
+        return cmd.ExecuteNonQuery();
+    }
+
+    public void Close()
+    {
+        conn.Close();
+    }
+}
